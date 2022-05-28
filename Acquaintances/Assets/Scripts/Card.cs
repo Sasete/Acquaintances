36268f14bcda0820903d3feadca6ef1bcf329a52
Sasete <T.TugraIskender@gmail.com>
2022-05-28 10:52:24 +0300

    UI imporvement
    Foldout Group for effect details.

--- Acquaintances/Assets/Scripts/Card.cs
+++ Acquaintances/Assets/Scripts/Card.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(fileName = "Card", menuName = "Acquaintances/Card", order = 2)]
 public class Card : ScriptableObject
@@ -12,8 +13,10 @@ public class Card : ScriptableObject
 
         public Trait trait;
 
+        [FoldoutGroup("Effect")]
         public bool includeCounter;
 
+        [FoldoutGroup("Effect")]
         public int effect;
 
     }
